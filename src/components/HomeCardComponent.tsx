ae497d698bf1ec643635dbc5826579d9a3e2f7e8
ClaireF <claire.fernandes.ee@gmail.com>
2025-02-06 15:23:32 +0100

    feat(homepage): create homePage

---
+++ src/components/HomeCardComponent.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { Card, CardContent, CardMedia, Typography, CardActions, Button } from "@mui/material";
+import { styled } from '@mui/material/styles';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+
+const PrimaryButton = styled(Button)({
+  boxShadow: 'none',
+  textTransform: 'none',
+  fontSize: 16,
+  padding: '6px 12px',
+  border: 'none',
+  lineHeight: 1.5,
+  backgroundColor: '#ea642a',
+  color: '#ffffff',
+  '&:hover': {
+    backgroundColor: '#ffffff',
+    border: '1px solid',
+    borderColor: '#ea642a',
+    color: '#ea642a',
+    boxShadow: 'none',
+  },
+  '&:active': {
+    boxShadow: 'none',
+    backgroundColor: '#f29a75',
+    color: '#333333',
+  },
+  '&:focus': {
+    boxShadow: 'none',
+    backgroundColor: '#f29a75',
+    color: '#333333',
+  },
+});
+
+export default function HomeCardComponent() {
+  return (
+    <Card sx={{ maxWidth: 345 }} variant="outlined">
+       <CardMedia
+        component="img"
+        height="140"
+        image="https://static.vecteezy.com/ti/vecteur-libre/p1/13160044-fond-d-ecran-de-vecteur-de-paysage-fond-de-printemps-ou-d-automne-vectoriel.jpg"
+        alt="Random Image"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          TodoList Name
+        </Typography>
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions sx={{ display: 'flex', justifyContent: 'end' }}>
+        <PrimaryButton endIcon={<ArrowForwardIcon />}>Voir la To do list</PrimaryButton>
+      </CardActions>
+    </Card>
+  );
+}
