419a1460b70c52241506418f107439da1341862b
Dercraker <antoine.capitain@gmail.com>
2025-02-06 16:53:03 +0100

    fix(Layout): move base layout from /page into /layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
-import Image from "next/image";
 import HomeCardComponent from "@/components/HomeCardComponent";
-import MainNavBar from "@/components/MainNavBar";
-
 export default function Home() {
   const cards = [];
 
@@ -9,59 +6,5 @@ export default function Home() {
     cards.push(<HomeCardComponent key={i} />);
   }
 
-  return (
-    <div className="font-[family-name:var(--font-geist-sans)]">
-      <MainNavBar />
-      <main className="flex items-center justify-center py-4">
-        <div className="grid grid-cols-4 gap-4">{cards}</div>
-      </main>
-      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+  return <div className="grid grid-cols-4 gap-4">{cards}</div>;
 }
