c4ba4711a2c92d407c484e9f15603c12eaf7e82a
Dercraker <antoine.capitain@gmail.com>
2025-02-07 11:41:00 +0100

    feat(Multi user todo ): Add somes methods

---
+++ src/features/todo/AddUserOnTodo.action.ts
@@ -0,0 +1,24 @@
+"use server";
+
+import { todoAction } from "@/lib/action/SafeAction";
+import { TodoMembershipRole } from "@prisma/client";
+import { z } from "zod";
+import { AddUserOnTodoQuery } from "./AddUserOnTodo.query";
+
+const AddUserOnTodoActionSchema = z.object({
+  roles: z.array(z.nativeEnum(TodoMembershipRole)).optional(),
+});
+
+export const AddUserOnTodoAction = todoAction
+  .schema(AddUserOnTodoActionSchema)
+  .action(async ({ parsedInput: { roles }, ctx }) => {
+    const todo = await AddUserOnTodoQuery({
+      userId: ctx.user.id,
+      roles,
+      where: {
+        id: ctx.todo.id,
+      },
+    });
+
+    return todo;
+  });
---
+++ src/features/todo/AddUserOnTodo.query.ts
@@ -0,0 +1,40 @@
+import { prisma } from "@/lib/prisma";
+import { TodoModel } from "@/types/prisma";
+import type { Prisma, TodoMembershipRole } from "@prisma/client";
+
+type AddUserOnTodoQueryProps = {
+  where: Prisma.TodoWhereUniqueInput;
+  userId: string;
+  roles?: TodoMembershipRole[];
+};
+
+export const AddUserOnTodoQuery = async ({
+  userId,
+  roles = ["MEMBER"],
+  where,
+}: AddUserOnTodoQueryProps) => {
+  const todo = await prisma.todo.update({
+    where: {
+      ...where,
+      members: {
+        none: {
+          userId,
+        },
+      },
+    },
+    data: {
+      members: {
+        create: {
+          roles,
+          user: {
+            connect: {
+              id: userId,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  return TodoModel.parse(todo);
+};
